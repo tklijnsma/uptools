6ba4407ad95951251c679f178ee9928930fd2aad
Thomas Klijnsma <tklijnsm@gmail.com>
2021-06-28 13:49:39 -0500

    switched to setuptools again

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("uptools/include/VERSION", "r") as f:
+    version = f.read().strip()
+
+setup(
+    name="uptools",
+    version=version,
+    license="BSD 3-Clause License",
+    description="Description text",
+    url="https://github.com/tklijnsma/uptools.git",
+    author="Thomas Klijnsma",
+    author_email="tklijnsm@gmail.com",
+    packages=["uptools"],
+    zip_safe=False,
+    install_requires=["seutils", "uproot-methods", "uproot3"],
+    include_package_data=True,
+    python_requires=">=3.5"
+)
---
+++ update_version.py
@@ -0,0 +1,20 @@
+# Script that updates the version in setup.py.
+from __future__ import print_function
+
+import os.path as osp
+
+
+def update_version():
+    version_file = osp.join(osp.dirname(__file__), "uptools/include/VERSION")
+    with open(version_file, "r") as f:
+        version = f.read().strip()
+    major, minor = version.rsplit(".", 1)
+    minor = str(int(minor) + 1)
+    updated_version = major + "." + minor
+    print("Updating {0} from {1} to {2}".format(version_file, version, updated_version))
+    with open(version_file, "w") as f:
+        f.write(updated_version)
+
+
+if __name__ == "__main__":
+    update_version()
